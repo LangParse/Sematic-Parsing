b34f35ad45eb5768795473a2b20ece6f89da3617
nphuoctho <nguyenphuoctho0310@gmail.com>
2025-08-18 17:24:23 +0700

    chore: update main.py

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 from src.amr_processor import AMRProcessor
-from src.amr_tokenizer import AMRTokenizer
 from src.amr_trainer import AMRTrainer
 
 
 def main():
     processor = AMRProcessor()
     dataset = processor.process_to_dataset(
-        train_path="./data/train/train_amr_2.txt", save_dir="./data/datasets"
+        train_path=["../data/train/train_amr_1.txt", "../data/train/train_amr_2.txt"]
     )
 
-    amr_tokenizer = AMRTokenizer(save_dir="./data/datasets")
-    tokenized_dataset = amr_tokenizer.tokenize_dataset(dataset, num_proc=4)
-
-    trainer = AMRTrainer(
-        dataset=tokenized_dataset, save_dir="./models/v1", use_wandb=True
-    )
+    trainer = AMRTrainer(dataset, use_wandb=True)
     trainer.train()
 
 
